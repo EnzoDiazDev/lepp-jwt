7ca2d270ebcfd84a249533b86ae6312e5362a11a
enzodiaz <enzodiazdev@gmail.com>
2020-11-10 19:02:47 -0300

    package details

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import Authentication from "./Authentication";
 
 export default Authentication;
+
+export {default as User} from "./utils/User";
+export {default as Storage} from "./utils/Storage";
