dc08faeeec013ad93a56f9a1a09795aff052d2a5
Lottie <enzodiazdev@gmail.com>
2020-11-30 18:27:38 -0300

    workflow  (#1)
    * cambios menores
    
    * autopublish

---
+++ index.ts
@@ -0,0 +1,6 @@
+import Authentication from "./src/Authentication";
+
+export default Authentication;
+
+export {default as User} from "./src/utils/User";
+export {default as Storage} from "./src/utils/Storage";
